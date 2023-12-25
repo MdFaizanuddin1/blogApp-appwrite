b98ee58a708255b9e78ae4bf5a23e9d2689e703f
MdFaizanuddin1 <md18faizan@gmail.com>
2023-12-25 23:30:41 +0530

    setup auth functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import conf from "./conf/conf";
 export default function App() {
-  console.log(conf.appwriteUrl);
+  //console.log(conf.appwriteUrl);
   return (
     <h1 className=" text-3xl font-bold underline">A Blog with appwrite!</h1>
   );

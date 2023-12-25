ea2569e36739b700f16d8e861f23afd3a299145a
MdFaizanuddin1 <md18faizan@gmail.com>
2023-12-25 21:18:22 +0530

    setup project , appwrite and env files

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import conf from "./conf/conf";
+export default function App() {
+  console.log(conf.appwriteUrl);
+  return (
+    <h1 className=" text-3xl font-bold underline">A Blog with appwrite!</h1>
+  );
+}

83ba9c5a335e72663746c4c1bc704eef7575fc01
MdFaizanuddin1 <md18faizan@gmail.com>
2023-12-26 15:26:16 +0530

    configure redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
+import { useState, useEffect } from "react";
 import "./App.css";
-import conf from "./conf/conf";
+import { useDispatch } from "react-redux";
+import authservice from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import { Outlet } from "react-router-dom";
+
 export default function App() {
-  //console.log(conf.appwriteUrl);
-  return (
-    <h1 className=" text-3xl font-bold underline">A Blog with appwrite!</h1>
+  const [loading, setLoading] = useState(true);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authservice
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className=" max-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className=" w-full block">
+        <Header />
+        <main>Todo {/* <Outlet /> */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : (
+    null
   );
 }

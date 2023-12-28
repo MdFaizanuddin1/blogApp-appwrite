013cf04ca15ee8db2abbadcf047e9c3d61db78a8
MdFaizanuddin1 <md18faizan@gmail.com>
2023-12-28 22:52:54 +0530

    build pages

--- src/components/index.js
+++ src/components/index.js
@@ -7,15 +7,24 @@ import Button from "./Button";
 import Input from "./Input";
 import RTE from "./RTE";
 import Select from "./Select";
-
+import Signup from "./Signup";
+import Login from "./Login";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
 export {
   Header,
   Footer,
   Container,
   Logo,
   LogoutBtn,
-  Button,
-  Input,
   RTE,
+  Signup,
+  Login,
+  Button,
+  PostForm,
+  PostCard,
+  AuthLayout,
+  Input,
   Select,
 };
